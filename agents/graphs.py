7bc28e1367b22e920db7ff2c774e598977ce9f95
dawalmalik0405-spec <dawalmalik0405@gmail.com>
2026-04-19 16:11:50 +0530

    h

--- agents/graphs.py
+++ agents/graphs.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph
 
-from ai_agentic_designer.node.nodes import page_node, ui_node, theme_node, asset_node, planner_node,figma_node
+from ai_agentic_designer.node.nodes import page_node, ui_node, theme_node, asset_node, planner_node
 from ai_agentic_designer.agents.state import AgentState
 
 
@@ -15,7 +15,7 @@ def create_agent_graph():
   workflow.add_node("ui", ui_node)
   workflow.add_node("theme", theme_node)
   workflow.add_node("assets", asset_node)
-  workflow.add_node("figma", figma_node)
+
 
   workflow.set_entry_point("plan")
 
@@ -23,9 +23,8 @@ def create_agent_graph():
   workflow.add_edge("page", "ui")
   workflow.add_edge("ui", "theme")
   workflow.add_edge("theme", "assets")
-  workflow.add_edge("assets", "figma")
 
-  workflow.set_finish_point("figma")
+  workflow.set_finish_point("assets")
 
   return workflow.compile()
 
