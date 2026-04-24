cbfd9e19fcf71e67753364d747f7911073ccc6ce
dawalmalik0405-spec <dawalmalik0405@gmail.com>
2026-04-24 23:15:30 +0530

    new node

--- agents/graphs.py
+++ agents/graphs.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph
 
-from ai_agentic_designer.node.nodes import page_node, ui_node, design_node, planner_node
+from ai_agentic_designer.node.nodes import page_node, ui_node, design_node, planner_node, code_node
 from ai_agentic_designer.agents.state import AgentState
 
 
@@ -14,6 +14,7 @@ def create_agent_graph():
   workflow.add_node("page", page_node)
   workflow.add_node("ui", ui_node)
   workflow.add_node("design", design_node)
+  workflow.add_node("code", code_node)
 
 
   workflow.set_entry_point("plan")
@@ -21,8 +22,9 @@ def create_agent_graph():
   workflow.add_edge("plan", "page")
   workflow.add_edge("page", "design")
   workflow.add_edge("design", "ui")
+  workflow.add_edge("ui","code")
 
-  workflow.set_finish_point("ui")
+  workflow.set_finish_point("code")
 
   return workflow.compile()
 
@@ -37,8 +39,8 @@ def run_graph(prompt):
   )
   return final_state
 
-# if __name__ == "__main__":
-#     main()
-
+if __name__ == "__main__":
+    result = run_graph("Create futuristic AI startup website")
+    print(result)
 
 
