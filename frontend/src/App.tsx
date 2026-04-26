c494733d0321b7ddecab0fa58f2c46c638feb099
dawalmalik0405-spec <dawalmalik0405@gmail.com>
2026-04-26 21:53:08 +0530

    g

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import ChatPanel from "./Components/ChatPanel"
 import PriviewPanel from "./Components/PreviewPanel"
+import { useState } from "react"
+
+
 
 
 export default function App() {
+  const [files, setFiles] = useState({})
+
   return (
-    <div className="flex h-screen bg-[#0f0f1a]">
-        <PriviewPanel />
-        <ChatPanel />
+    <div className="flex h-screen w-screen overflow-hidden bg-[#0f0f1a]">
+        
+        <ChatPanel setFiles={setFiles}/>
+        <PriviewPanel files={files} />
     </div>
   )
 }
